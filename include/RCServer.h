93ffef3d3241eddde2d8f6d36fcf4884a71d033c
Tuomo <tuokri@tuta.io>
2022-02-18 23:51:16 +0200

    Tabs to spaces

--- include/RCServer.h
+++ include/RCServer.h
@@ -10,47 +10,47 @@
 
 enum class EPacketID
 {
-	None						= 0,
-	PlayerMeta 					= 1,
-	PlayerStoppedSpeaking       = 2,
-	PlayerSpeaking              = 3,
-	PlayerSpeakingSpatialized   = 4,
-	PlayerDisconnected 			= 5,
-	PlayerSpawned 				= 6,
-	PlayerDied 					= 7,
-	PlayerSquadChanged 			= 8,
-	PlayerTeamChanged 			= 9,
+    None						= 0,
+    PlayerMeta 					= 1,
+    PlayerStoppedSpeaking       = 2,
+    PlayerSpeaking              = 3,
+    PlayerSpeakingSpatialized   = 4,
+    PlayerDisconnected 			= 5,
+    PlayerSpawned 				= 6,
+    PlayerDied 					= 7,
+    PlayerSquadChanged 			= 8,
+    PlayerTeamChanged 			= 9,
 };
 
 enum class ETeam
 {
-	Axis 	= 0,
-	Allies 	= 1,
-	None    = 255,
+    Axis 	= 0,
+    Allies 	= 1,
+    None    = 255,
 };
 
 class Packet
 {
 public:
-	Packet(): id(EPacketID::None) {};
-	virtual ~Packet() = default;
-	inline static uint32_t minDataSize() {return HEADER_SIZE;};
+    Packet(): id(EPacketID::None) {};
+    virtual ~Packet() = default;
+    inline static uint32_t minDataSize() {return HEADER_SIZE;};
 
 public:
-	static constexpr uint8_t HEADER_SIZE = sizeof(uint8_t) * 2;
-	EPacketID id;
+    static constexpr uint8_t HEADER_SIZE = sizeof(uint8_t) * 2;
+    EPacketID id;
 };
 
 class PlayerMeta: public Packet
 {
 public:
-	inline static uint32_t minDataSize();
+    inline static uint32_t minDataSize();
 
 public:
-	ETeam team;
-	uint8_t squad;
-	uint32_t playerId;
-	uint64_t uniqueNetId;
+    ETeam team;
+    uint8_t squad;
+    uint32_t playerId;
+    uint64_t uniqueNetId;
 };
 
 class PlayerStoppedSpeaking: public Packet{};
@@ -65,49 +65,49 @@ class PlayerTeamChanged: public Packet{};
 /// Rising Storm 2: Vietnam dedicated server remote control.
 class RCServer: public QTcpServer
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	explicit RCServer(QObject* parent = nullptr):
-		  QTcpServer(parent)
-	{}
+    explicit RCServer(QObject* parent = nullptr):
+          QTcpServer(parent)
+    {}
 
 protected:
-	void incomingConnection(qintptr socketDescriptor) override;
+    void incomingConnection(qintptr socketDescriptor) override;
 
 public slots:
-	void startServer();
-	void closeServer();
+    void startServer();
+    void closeServer();
 };
 
 class RCPacketHandler : public QObject
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	explicit RCPacketHandler(qintptr socketDescriptor, QObject* parent = nullptr);
+    explicit RCPacketHandler(qintptr socketDescriptor, QObject* parent = nullptr);
 
 private:
-	static inline PlayerMeta handlePlayerMeta(QDataStream& dataStream);
-	static inline PlayerStoppedSpeaking handlePlayerStoppedSpeaking(QByteArray& data);
-	static inline PlayerSpeaking handlePlayerSpeaking(QByteArray& data);
-	static inline PlayerSpeakingSpatialized handlePlayerSpeakingSpatialized(QByteArray& data);
-	static inline PlayerDisconnected handlePlayerDisconnected(QByteArray& data);
-	static inline PlayerSpawned handlePlayerSpawned(QByteArray& data);
-	static inline PlayerDied handlePlayerDied(QByteArray& data);
-	static inline PlayerSquadChanged handlePlayerSquadChanged(QByteArray& data);
-	static inline PlayerTeamChanged handlePlayerTeamChanged(QByteArray& data);
+    static inline PlayerMeta handlePlayerMeta(QDataStream& dataStream);
+    static inline PlayerStoppedSpeaking handlePlayerStoppedSpeaking(QByteArray& data);
+    static inline PlayerSpeaking handlePlayerSpeaking(QByteArray& data);
+    static inline PlayerSpeakingSpatialized handlePlayerSpeakingSpatialized(QByteArray& data);
+    static inline PlayerDisconnected handlePlayerDisconnected(QByteArray& data);
+    static inline PlayerSpawned handlePlayerSpawned(QByteArray& data);
+    static inline PlayerDied handlePlayerDied(QByteArray& data);
+    static inline PlayerSquadChanged handlePlayerSquadChanged(QByteArray& data);
+    static inline PlayerTeamChanged handlePlayerTeamChanged(QByteArray& data);
 
 private slots:
-	void readSocketData();
-	void disconnected();
+    void readSocketData();
+    void disconnected();
 
 signals:
-	void resultReady();
-	void error(QTcpSocket::SocketError socketError);
+    void resultReady();
+    void error(QTcpSocket::SocketError socketError);
 
 private:
-	QTcpSocket* sock = nullptr;
+    QTcpSocket* sock = nullptr;
 };
 
 // TODO: read socket, parse bytes and forward packet object
@@ -115,7 +115,7 @@ private:
 // TODO: rename? RCIOTask? RCSocketTask?
 class RCPacketTask: public QObject, public QRunnable
 {
-	Q_OBJECT
+    Q_OBJECT
 
 };
 
--- src/RCServer.cpp
+++ src/RCServer.cpp
@@ -11,166 +11,166 @@ constexpr int BUFFER_SIZE = 255;
 
 QDataStream& operator>>(QDataStream& in, EPacketID& packetId)
 {
-	return in >> reinterpret_cast<uint8_t&>(packetId);
+    return in >> reinterpret_cast<uint8_t&>(packetId);
 }
 
 std::ostream& operator<<(std::ostream& os, EPacketID& packetId)
 {
-	switch (packetId)
-	{
-		case EPacketID::None:
-			return os << "EPacketID::None";
-		case EPacketID::PlayerMeta:
-			return os << "EPacketID::PlayerMeta";
-		case EPacketID::PlayerStoppedSpeaking:
-			return os << "EPacketID::PlayerStoppedSpeaking";
-		case EPacketID::PlayerSpeaking:
-			return os << "EPacketID::PlayerSpeaking";
-		case EPacketID::PlayerSpeakingSpatialized:
-			return os << "EPacketID::PlayerSpeakingSpatialized";
-		case EPacketID::PlayerDisconnected:
-			return os << "EPacketID::PlayerDisconnected";
-		case EPacketID::PlayerSpawned:
-			return os << "EPacketID::PlayerSpawned";
-		case EPacketID::PlayerDied:
-			return os << "EPacketID::PlayerDied";
-		case EPacketID::PlayerSquadChanged:
-			return os << "EPacketID::PlayerSquadChanged";
-		case EPacketID::PlayerTeamChanged:
-			return os << "EPacketID::PlayerTeamChanged";
-	}
-	return os << static_cast<uint32_t>(packetId);
+    switch (packetId)
+    {
+        case EPacketID::None:
+            return os << "EPacketID::None";
+        case EPacketID::PlayerMeta:
+            return os << "EPacketID::PlayerMeta";
+        case EPacketID::PlayerStoppedSpeaking:
+            return os << "EPacketID::PlayerStoppedSpeaking";
+        case EPacketID::PlayerSpeaking:
+            return os << "EPacketID::PlayerSpeaking";
+        case EPacketID::PlayerSpeakingSpatialized:
+            return os << "EPacketID::PlayerSpeakingSpatialized";
+        case EPacketID::PlayerDisconnected:
+            return os << "EPacketID::PlayerDisconnected";
+        case EPacketID::PlayerSpawned:
+            return os << "EPacketID::PlayerSpawned";
+        case EPacketID::PlayerDied:
+            return os << "EPacketID::PlayerDied";
+        case EPacketID::PlayerSquadChanged:
+            return os << "EPacketID::PlayerSquadChanged";
+        case EPacketID::PlayerTeamChanged:
+            return os << "EPacketID::PlayerTeamChanged";
+    }
+    return os << static_cast<uint32_t>(packetId);
 }
 
 QDataStream& operator<<(QDataStream& out, EPacketID& packetId)
 {
-	std::stringstream ss;
-	ss << packetId;
-	return out << ss.get();
+    std::stringstream ss;
+    ss << packetId;
+    return out << ss.get();
 }
 
 QDebug operator<<(QDebug debug, EPacketID& packetId)
 {
-	std::stringstream ss;
-	ss << packetId;
-	return debug << ss.get();
+    std::stringstream ss;
+    ss << packetId;
+    return debug << ss.get();
 }
 
 void RCServer::incomingConnection(qintptr socketDescriptor)
 {
-	new RCPacketHandler(socketDescriptor, this);
+    new RCPacketHandler(socketDescriptor, this);
 }
 
 void RCServer::startServer()
 {
-	listen(QHostAddress("localhost"), 10053);
+    listen(QHostAddress("localhost"), 10053);
 }
 
 void RCServer::closeServer()
 {
-	close();
+    close();
 }
 
 RCPacketHandler::RCPacketHandler(
-	qintptr socketDescriptor, QObject *parent): QObject(parent)
+    qintptr socketDescriptor, QObject *parent): QObject(parent)
 {
-	sock = new QTcpSocket{};
-	if (!sock->setSocketDescriptor(socketDescriptor))
-	{
-		emit error(sock->error());
-		return;
-	}
+    sock = new QTcpSocket{};
+    if (!sock->setSocketDescriptor(socketDescriptor))
+    {
+        emit error(sock->error());
+        return;
+    }
 
-	connect(sock, &QTcpSocket::readyRead,
-			this, &RCPacketHandler::readSocketData, Qt::DirectConnection);
-	connect(sock, &QTcpSocket::disconnected,
-			this, &RCPacketHandler::disconnected);
+    connect(sock, &QTcpSocket::readyRead,
+            this, &RCPacketHandler::readSocketData, Qt::DirectConnection);
+    connect(sock, &QTcpSocket::disconnected,
+            this, &RCPacketHandler::disconnected);
 }
 
 void RCPacketHandler::readSocketData()
 {
-	QByteArray data;
-	data = sock->read(BUFFER_SIZE);
+    QByteArray data;
+    data = sock->read(BUFFER_SIZE);
 
-	QHostAddress peerAddr = sock->peerAddress();
-	qWarning() << peerAddr << " data: " << data;
+    QHostAddress peerAddr = sock->peerAddress();
+    qWarning() << peerAddr << " data: " << data;
 
-	if (data.size() < Packet::minDataSize())
-	{
-		qWarning() << "not enough data received";
-		return;
-	}
+    if (data.size() < Packet::minDataSize())
+    {
+        qWarning() << "not enough data received";
+        return;
+    }
 
-	QDataStream dataStream{data};
-	// Sending from UnrealScript side in little endian.
-	dataStream.setByteOrder(QDataStream::LittleEndian);
+    QDataStream dataStream{data};
+    // Sending from UnrealScript side in little endian.
+    dataStream.setByteOrder(QDataStream::LittleEndian);
 
-	uint32_t size;
-	EPacketID packetId;
-	// size = static_cast<uint32_t>(static_cast<uint8_t>(data[0]));a
-	// packetId = static_cast<EPacketID>(static_cast<uint8_t>(data[1]));
-	dataStream >> size;
-	dataStream >> packetId;
-	qWarning() << "device.size = " << dataStream.device()->size();
-	qWarning() << "size = " << size;
-	qWarning() << "packetId = " << packetId;
+    uint32_t size;
+    EPacketID packetId;
+    // size = static_cast<uint32_t>(static_cast<uint8_t>(data[0]));a
+    // packetId = static_cast<EPacketID>(static_cast<uint8_t>(data[1]));
+    dataStream >> size;
+    dataStream >> packetId;
+    qWarning() << "device.size = " << dataStream.device()->size();
+    qWarning() << "size = " << size;
+    qWarning() << "packetId = " << packetId;
 
-	switch (packetId)
-	{
-		case EPacketID::PlayerMeta:
-			handlePlayerMeta(dataStream);
-			break;
-		case EPacketID::PlayerStoppedSpeaking:
-			handlePlayerStoppedSpeaking(data);
-			break;
-		case EPacketID::PlayerSpeaking:
-			handlePlayerSpeaking(data);
-			break;
-		case EPacketID::PlayerSpeakingSpatialized:
-			handlePlayerSpeakingSpatialized(data);
-			break;
-		case EPacketID::PlayerDisconnected:
-			handlePlayerDisconnected(data);
-			break;
-		case EPacketID::PlayerSpawned:
-			handlePlayerSpawned(data);
-			break;
-		case EPacketID::PlayerDied:
-			handlePlayerDied(data);
-			break;
-		case EPacketID::PlayerSquadChanged:
-			handlePlayerSquadChanged(data);
-			break;
-		case EPacketID::PlayerTeamChanged:
-			handlePlayerTeamChanged(data);
-			break;
-		case EPacketID::None:
-		default:
-			break;
-	}
+    switch (packetId)
+    {
+        case EPacketID::PlayerMeta:
+            handlePlayerMeta(dataStream);
+            break;
+        case EPacketID::PlayerStoppedSpeaking:
+            handlePlayerStoppedSpeaking(data);
+            break;
+        case EPacketID::PlayerSpeaking:
+            handlePlayerSpeaking(data);
+            break;
+        case EPacketID::PlayerSpeakingSpatialized:
+            handlePlayerSpeakingSpatialized(data);
+            break;
+        case EPacketID::PlayerDisconnected:
+            handlePlayerDisconnected(data);
+            break;
+        case EPacketID::PlayerSpawned:
+            handlePlayerSpawned(data);
+            break;
+        case EPacketID::PlayerDied:
+            handlePlayerDied(data);
+            break;
+        case EPacketID::PlayerSquadChanged:
+            handlePlayerSquadChanged(data);
+            break;
+        case EPacketID::PlayerTeamChanged:
+            handlePlayerTeamChanged(data);
+            break;
+        case EPacketID::None:
+        default:
+            break;
+    }
 }
 
 // TODO: don't return anything and handle the data right away.
 PlayerMeta RCPacketHandler::handlePlayerMeta(QDataStream& dataStream)
 {
-	if (dataStream.device()->size() < PlayerMeta::minDataSize())
-	{
-		qWarning() << "not enough data for PlayerMeta";
-		return {};
-	}
+    if (dataStream.device()->size() < PlayerMeta::minDataSize())
+    {
+        qWarning() << "not enough data for PlayerMeta";
+        return {};
+    }
 
-	PlayerMeta pm;
-	uint32_t netIdA;
-	uint32_t netIdB;
+    PlayerMeta pm;
+    uint32_t netIdA;
+    uint32_t netIdB;
 
-	dataStream >> pm.team;
-	dataStream >> pm.squad;
-	dataStream >> pm.playerId;
-	dataStream >> netIdA;
-	dataStream >> netIdB;
-	// (B << 32) | A
-	pm.uniqueNetId = (static_cast<uint64_t>(netIdB) << 32) | netIdA;
-	// dataStream >> pm.uniqueNetId;
+    dataStream >> pm.team;
+    dataStream >> pm.squad;
+    dataStream >> pm.playerId;
+    dataStream >> netIdA;
+    dataStream >> netIdB;
+    // (B << 32) | A
+    pm.uniqueNetId = (static_cast<uint64_t>(netIdB) << 32) | netIdA;
+    // dataStream >> pm.uniqueNetId;
 //	pm.team = static_cast<ETeam>(static_cast<uint8_t>(data[2]));
 //	pm.squad = static_cast<uint8_t>(data[3]);
 //	pm.playerId = qFromBigEndian(
@@ -190,55 +190,55 @@ PlayerMeta RCPacketHandler::handlePlayerMeta(QDataStream& dataStream)
 //		 static_cast<uint64_t>(data[15]) << 24
 //		 ));
 
-	qWarning() << "team          " << static_cast<uint8_t>(pm.team);
-	qWarning() << "squad         " << pm.squad;
-	qWarning() << "playerId      " << pm.playerId;
-	qWarning() << "uniqueNetId   " << pm.uniqueNetId;
+    qWarning() << "team          " << static_cast<uint8_t>(pm.team);
+    qWarning() << "squad         " << pm.squad;
+    qWarning() << "playerId      " << pm.playerId;
+    qWarning() << "uniqueNetId   " << pm.uniqueNetId;
 
-	// TODO: Do murmur specific things with PlayerMeta data...
-	//		 emit result?
+    // TODO: Do murmur specific things with PlayerMeta data...
+    //		 emit result?
 
-	return pm;
+    return pm;
 }
 
 PlayerStoppedSpeaking RCPacketHandler::handlePlayerStoppedSpeaking(QByteArray &data)
 {
-	return {};
+    return {};
 }
 
 PlayerSpeaking RCPacketHandler::handlePlayerSpeaking(QByteArray &data)
 {
-	return {};
+    return {};
 }
 
 PlayerSpeakingSpatialized RCPacketHandler::handlePlayerSpeakingSpatialized(QByteArray &data)
 {
-	return {};
+    return {};
 }
 
 PlayerDisconnected RCPacketHandler::handlePlayerDisconnected(QByteArray &data)
 {
-	return {};
+    return {};
 }
 
 PlayerSpawned RCPacketHandler::handlePlayerSpawned(QByteArray &data)
 {
-	return {};
+    return {};
 }
 
 PlayerDied RCPacketHandler::handlePlayerDied(QByteArray &data)
 {
-	return {};
+    return {};
 }
 
 PlayerSquadChanged RCPacketHandler::handlePlayerSquadChanged(QByteArray &data)
 {
-	return {};
+    return {};
 }
 
 PlayerTeamChanged RCPacketHandler::handlePlayerTeamChanged(QByteArray &data)
 {
-	return {};
+    return {};
 }
 
 void RCPacketHandler::disconnected()
@@ -248,9 +248,9 @@ void RCPacketHandler::disconnected()
 
 uint32_t PlayerMeta::minDataSize()
 {
-	return Packet::minDataSize()
-		   + sizeof(uint8_t)
-		   + sizeof(uint8_t)
-		   + sizeof(uint32_t)
-		   + sizeof(uint64_t);
+    return Packet::minDataSize()
+           + sizeof(uint8_t)
+           + sizeof(uint8_t)
+           + sizeof(uint32_t)
+           + sizeof(uint64_t);
 }
